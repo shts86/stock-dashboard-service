a8aa0f2f858f27c1c3bfdc04942d7120cc5f2480
Shahar T <shts86@gmail.com>
2022-03-29 10:41:52 +0300

    fix crash

--- src/routes/all-stocks.ts
+++ src/routes/all-stocks.ts
@@ -33,49 +33,4 @@ router.get(
   }),
 );
 
-router.post(
-  '/',
-  wrapAsync(async (req, res) => {
-    const stock = getOrThrow<Stock>(req.body, newStockSchema);
-    const store = resolveStore(res);
-    await store.allStocks.add([stock]);
-    res.status(201).send(stock);
-  }),
-);
-
-// TODO - use code param and not _id
-// router.put(
-//   '/:code',
-//   wrapAsync(async (req, res) => {
-//     const id = getOrThrow<string>(req.params.id, idSchema);
-//     const stock = getOrThrow<Stock>(req.body, stockSchemaCode);
-//     stock._id = id;
-
-//     const store = resolveStore(res);
-//     const replaced = await store.allStocks.replace(stock);
-//     if (!replaced) {
-//       res.sendStatus(404);
-//       return;
-//     }
-
-//     res.send(stock);
-//   }),
-// );
-
-// router.delete(
-//   '/:code',
-//   wrapAsync(async (req, res) => {
-//     const code = getOrThrow<string>(req.params.id, idSchema);
-//     const store = resolveStore(res);
-
-//     const deleted = await store.allStocks.deleteById(code);
-//     if (!deleted) {
-//       res.sendStatus(404);
-//       return;
-//     }
-
-//     res.sendStatus(204);
-//   }),
-// );
-
 export default router;
